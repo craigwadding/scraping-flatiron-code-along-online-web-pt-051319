4121b8365083b96487f365b9a5be60abe272ce36
craig wadding <craig.wadding@gmail.com>
2019-06-26 01:11:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,16 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    
-  end 
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+  end
+ 
+end
+ 
+Scraper.new.get_page
+
+  
   
     def print_courses
     self.make_courses
